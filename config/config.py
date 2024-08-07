7221ec2e1347d44af85b1627765d8ad68626db48
aruseus98 <aruseus98@gmail.com>
2024-08-07 18:11:52 +0200

    Setup to check every 15mins

--- config/config.py
+++ config/config.py
@@ -1,8 +1,16 @@
 import os
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 DISCORD_USER_ID = os.getenv('DISCORD_USER_ID')
-RSS_FEED_URL = os.getenv('RSS_FEED_URL')
+RSS_FEED_URLS = os.getenv('RSS_FEED_URLS').split(',')
+
+# Création du répertoire logs si nécessaire
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+# Configuration des logs
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s', filename='logs/bot.log', filemode='a')
--- utils/functions.py
+++ utils/functions.py
@@ -1,5 +1,31 @@
 import feedparser
+import aiofiles
+import os
+import logging
+
+LAST_ENTRY_DIR = 'last_entry'
+
+# Assurer la création du répertoire last_entry
+if not os.path.exists(LAST_ENTRY_DIR):
+    os.makedirs(LAST_ENTRY_DIR)
 
 def get_rss_feed(url):
     feed = feedparser.parse(url)
     return feed.entries
+
+async def read_last_entry(feed_url):
+    file_path = os.path.join(LAST_ENTRY_DIR, f"{hash(feed_url)}.txt")
+    try:
+        async with aiofiles.open(file_path, 'r') as file:
+            last_entry = await file.read()
+            logging.info("Read last entry for %s: %s", feed_url, last_entry.strip())
+            return last_entry.strip()
+    except FileNotFoundError:
+        logging.warning("Last entry file for %s not found.", feed_url)
+        return None
+
+async def write_last_entry(feed_url, entry_id):
+    file_path = os.path.join(LAST_ENTRY_DIR, f"{hash(feed_url)}.txt")
+    async with aiofiles.open(file_path, 'w') as file:
+        await file.write(entry_id)
+        logging.info("Wrote last entry for %s: %s", feed_url, entry_id)

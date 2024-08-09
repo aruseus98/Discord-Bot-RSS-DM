fcb26b31d681086b8c15ac76d42d22d2c365bbd2
aruseus98 <aruseus98@gmail.com>
2024-08-09 17:27:11 +0200

    Add TRUE/FALSE DEBUG MODE

--- config/config.py
+++ config/config.py
@@ -8,9 +8,23 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 DISCORD_CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))  # Assurez-vous que l'ID du canal est un entier
 RSS_FEED_URLS = os.getenv('RSS_FEED_URLS').split(',')
 
-# Création du répertoire logs si nécessaire
-if not os.path.exists('logs'):
-    os.makedirs('logs')
+# Lecture de la variable DEBUG_MODE à partir du fichier .env
+DEBUG_MODE = os.getenv('DEBUG_MODE', 'false').lower() == 'true'
 
-# Configuration des logs
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s', filename='logs/bot.log', filemode='a')
+if DEBUG_MODE:
+    # Création du répertoire logs si nécessaire
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+    
+    # Configuration des logs pour le mode DEBUG
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s %(levelname)s:%(message)s',
+        filename='logs/bot.log',
+        filemode='a'
+    )
+    logging.info("Logging is set to DEBUG mode.")
+else:
+    # Configuration des logs pour ignorer tous les messages
+    logging.getLogger().addHandler(logging.NullHandler())
+    logging.info("Logging is disabled in production mode.")
